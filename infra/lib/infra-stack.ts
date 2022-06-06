706a6630ae0e058d7642dab455a6d18183636de2
Mehdi Ayache <mehdi@noie.com>
2022-06-06 12:16:51 +0200

    :hammer: running

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,7 +3,8 @@ import { Construct } from "constructs";
 import * as path from "path";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-
+import { WebSocketLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -15,6 +16,21 @@ export class InfraStack extends Stack {
       bundling: {
         minify: true,
         externalModules: ["aws-sdk"],
+      },
+    });
+
+    const webSocketApi = new apigwv2.WebSocketApi(this, "Websocket API", {
+      connectRouteOptions: {
+        integration: new WebSocketLambdaIntegration("ConnectIntegration", fn),
+      },
+      disconnectRouteOptions: {
+        integration: new WebSocketLambdaIntegration(
+          "DisconnectIntegration",
+          fn
+        ),
+      },
+      defaultRouteOptions: {
+        integration: new WebSocketLambdaIntegration("DefaultIntegration", fn),
       },
     });
   }
